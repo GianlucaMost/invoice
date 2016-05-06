310820573a1db6bbe17e71c0b35727d197b8cd67
Gianluca <most.gianluca@gmail.com>
2016-05-06 16:34:15 +0200

    hotfix for exception handling

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ import logic.Auto;
 import logic.Kunde;
 import ui.GUI;
 
+import javax.xml.bind.JAXBException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +10,7 @@ import java.util.List;
  * Created by gianlucamost on 03.05.16.
  */
 public class main {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws JAXBException {
 
 /*
         Kunde kunde = new Kunde("Mustermann", "Max", 123, "Musterstr.");
